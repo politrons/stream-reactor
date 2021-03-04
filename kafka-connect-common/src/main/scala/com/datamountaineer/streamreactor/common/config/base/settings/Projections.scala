5a330cd465fc10898496ff4b19e55418cbcb3949
Andrew Stevenson <andrew@lenses.io>
2021-03-04 18:23:23 +0100

    refactor

--- kafka-connect-common/src/main/scala/com/datamountaineer/streamreactor/common/config/base/settings/Projections.scala
+++ kafka-connect-common/src/main/scala/com/datamountaineer/streamreactor/common/config/base/settings/Projections.scala
@@ -18,7 +18,7 @@
 
 package com.datamountaineer.streamreactor.common.config.base.settings
 
-import com.datamountaineer.kcql.{Bucketing, FormatType, Kcql, Tag, WriteModeEnum}
+import com.datamountaineer.kcql._
 import com.datamountaineer.streamreactor.common.errors.{ErrorPolicy, ErrorPolicyEnum}
 import com.datamountaineer.streamreactor.connect.converters.source.Converter
 import com.typesafe.scalalogging.StrictLogging
@@ -47,22 +47,27 @@ case class Projections(targets: Map[String, String], // source -> target
                        tags: Map[String, Set[Tag]] = Map.empty,
                        withType: Map[String, String] = Map.empty,
                        dynamicTarget: Map[String, String] = Map.empty,
-                       errorPolicy: ErrorPolicy = ErrorPolicy(ErrorPolicyEnum.THROW),
+                       errorPolicy: ErrorPolicy = ErrorPolicy(
+                         ErrorPolicyEnum.THROW),
                        errorRetries: Int = 0,
-                       kcqls: Set[Kcql] = Set.empty
-                      )
-
-
+                       kcqls: Set[Kcql] = Set.empty)
 
 object Projections extends StrictLogging {
-  def apply(kcqls: Set[Kcql], props: Map[String, String], errorPolicy: ErrorPolicy, errorRetries: Int, defaultBatchSize: Int): Projections = {
+  def apply(kcqls: Set[Kcql],
+            sourceConverterProps: Map[String, String],
+            errorPolicy: ErrorPolicy,
+            errorRetries: Int,
+            defaultBatchSize: Int): Projections = {
     Projections(
       targets = getTargetMapping(kcqls),
       writeMode = getWriteMode(kcqls),
       headerFields = getHeaderFields(kcqls),
       keyFields = getKeyFields(kcqls),
       valueFields = getValueFields(kcqls),
-      ignoreFields = kcqls.map(rm => (rm.getSource, rm.getIgnoredFields.asScala.map(f => f.getName).toSet)).toMap,
+      ignoreFields = kcqls
+        .map(rm =>
+          (rm.getSource, rm.getIgnoredFields.asScala.map(f => f.getName).toSet))
+        .toMap,
       primaryKeys = getPrimaryKeyCols(kcqls),
       formats = getFormat(kcqls),
       partitionBy = getPartitionByFields(kcqls),
@@ -72,7 +77,6 @@ object Projections extends StrictLogging {
       batchSize = getBatchSize(kcqls, defaultBatchSize),
       tags = getTags(kcqls),
       withType = getWithType(kcqls),
-      sourceConverters = getSourceConverters(kcqls, props),
       errorPolicy = errorPolicy,
       errorRetries = errorRetries,
       kcqls = kcqls
@@ -95,7 +99,7 @@ object Projections extends StrictLogging {
     kcql.toList
       .map(k =>
         (k.getSource,
-          ListSet(k.getPrimaryKeys.asScala.map(p => p.getName).reverse: _*).toSet))
+         ListSet(k.getPrimaryKeys.asScala.map(p => p.getName).reverse: _*).toSet))
       .toMap
   }
 
@@ -106,12 +110,13 @@ object Projections extends StrictLogging {
       .toMap
   }
 
-
   def getFormat(kcql: Set[Kcql]): Map[String, FormatType] = {
     kcql.toList.map(r => (r.getSource, r.getFormatType)).toMap
   }
 
-  def getUpsertKeys(kcqls: Set[Kcql], preserveFullKeys: Boolean = false): Map[String, Set[String]] = {
+  def getUpsertKeys(
+      kcqls: Set[Kcql],
+      preserveFullKeys: Boolean = false): Map[String, Set[String]] = {
 
     kcqls
       .filter(c => c.getWriteMode == WriteModeEnum.UPSERT)
@@ -123,7 +128,7 @@ object Projections extends StrictLogging {
                 key.toString
               } else {
                 key.getName
-              })
+            })
             .reverse: _*)
         if (keys.isEmpty)
           throw new ConfigException(
@@ -137,8 +142,10 @@ object Projections extends StrictLogging {
     kcql.toList.map(r => (r.getSource, r.getWriteMode)).toMap
   }
 
-  def getTargetMapping(kcql: Set[Kcql]) : Map[String, String] = {
-    kcql.map { k => k.getSource -> k.getTarget}.toMap
+  def getTargetMapping(kcql: Set[Kcql]): Map[String, String] = {
+    kcql.map { k =>
+      k.getSource -> k.getTarget
+    }.toMap
   }
 
   def getValueFields(kcql: Set[Kcql]): Map[String, Map[String, String]] = {
@@ -171,7 +178,8 @@ object Projections extends StrictLogging {
           }
 
           name.replaceFirst("_key.", "") -> f.getAlias
-        }).toMap
+        })
+        .toMap
     }.toMap
   }
 
@@ -187,43 +195,62 @@ object Projections extends StrictLogging {
           }
 
           name.replaceFirst("_header.", "") -> f.getAlias
-        }).toMap
+        })
+        .toMap
     }.toMap
   }
 
   def getPartitionByFields(kcql: Set[Kcql]): Map[String, Set[String]] = {
-    kcql.map { k => (k.getSource -> k.getPartitionBy.asScala.toSet) }.toMap
+    kcql.map { k =>
+      (k.getSource -> k.getPartitionBy.asScala.toSet)
+    }.toMap
   }
 
   def getTags(kcql: Set[Kcql]): Map[String, Set[Tag]] = {
-    kcql.map { k => (k.getSource -> k.getTags.asScala.toSet) }.toMap
+    kcql.map { k =>
+      (k.getSource -> k.getTags.asScala.toSet)
+    }.toMap
   }
 
   def getWithType(kcql: Set[Kcql]): Map[String, String] = {
-    kcql.map { k => (k.getSource -> k.getWithType) }.toMap
+    kcql.map { k =>
+      (k.getSource -> k.getWithType)
+    }.toMap
   }
 
   def getDynamicTarget(kcql: Set[Kcql]): Map[String, String] = {
-    kcql.map { k => (k.getSource -> k.getDynamicTarget) }.toMap
+    kcql.map { k =>
+      (k.getSource -> k.getDynamicTarget)
+    }.toMap
   }
 
   def getDocType(kcql: Set[Kcql]): Map[String, String] = {
-    kcql.map { k => (k.getSource -> k.getDocType) }.toMap
+    kcql.map { k =>
+      (k.getSource -> k.getDocType)
+    }.toMap
   }
 
   def getBucketing(kcql: Set[Kcql]): Map[String, Bucketing] = {
-    kcql.map { k => (k.getSource -> k.getBucketing) }.toMap
+    kcql.map { k =>
+      (k.getSource -> k.getBucketing)
+    }.toMap
   }
 
   def getKeyDelimiter(kcql: Set[Kcql]): Map[String, String] = {
-    kcql.map { k => (k.getSource -> k.getKeyDelimeter) }.toMap
+    kcql.map { k =>
+      (k.getSource -> k.getKeyDelimeter)
+    }.toMap
   }
 
   def getWithKeys(kcql: Set[Kcql]): Map[String, Set[String]] = {
-    kcql.map { k => (k.getSource -> k.getWithKeys.asScala.toSet) }.toMap
+    kcql.map { k =>
+      (k.getSource -> k.getWithKeys.asScala.toSet)
+    }.toMap
   }
 
-  def getSourceConverters(kcql: Set[Kcql], props: Map[String, String]): Map[String, Converter] = {
+  def getSourceConverters(
+      kcql: Set[Kcql],
+      props: Map[String, String]): Map[String, Converter] = {
     kcql
       .filterNot(k => k.getWithConverter == null)
       .map(k => (k.getSource, k.getWithConverter))
@@ -232,10 +259,13 @@ object Projections extends StrictLogging {
           logger.info(s"Creating converter instance for $clazz")
           val converter = Try(Class.forName(clazz).newInstance()) match {
             case Success(value) => value.asInstanceOf[Converter]
-            case Failure(_) => throw new ConfigException(s"Invalid KCQL is invalid for [$source]. [$clazz] should have an empty ctor!")
+            case Failure(_) =>
+              throw new ConfigException(
+                s"Invalid KCQL is invalid for [$source]. [$clazz] should have an empty ctor!")
           }
           converter.initialize(props)
           (source, converter)
-    }).toMap
+      })
+      .toMap
   }
 }
